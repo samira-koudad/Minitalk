f4abc26eae470dff31ee66c87f8acaa782c6c8ae
Samira K. <samiirakoudad@gmail.com>
2025-06-08 19:27:06 +0200

    Add files via upload

---
+++ includes/minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skoudad <skoudad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 16:50:21 by skoudad           #+#    #+#             */
+/*   Updated: 2025/06/06 11:21:42 by skoudad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <unistd.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "../includes/ft_printf/ft_printf.h"
+# include "../includes/libft/libft.h"
+
+#endif
---
+++ includes/minitalk_bonus.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skoudad <skoudad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/05 21:00:29 by skoudad           #+#    #+#             */
+/*   Updated: 2025/06/06 11:21:53 by skoudad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "../includes/ft_printf/ft_printf.h"
+# include "../includes/libft/libft.h"
+
+#endif
